cc5dc39971850c3da304c7bc4d420fdb9d238ad0
yunoXL7 <102035393+yunoXL@users.noreply.github.com>
2026-02-01 10:20:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'flow-v1';
+const assets = ['./index.html', './financas.html', './agenda.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
